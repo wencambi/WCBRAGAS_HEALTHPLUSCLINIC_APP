8ec44bd4985ccc0aa6423cfe9c5c0955d5a5b040
Jayson <kanekiken101017@gmail.com>
2023-11-02 09:37:31 +0800

    fixed nextButton & modified manifest
    Signed-off-by: Jayson <kanekiken101017@gmail.com>

--- app/src/main/java/com/example/wcbragas_healthplusclinic_app/MainActivity.java
+++ app/src/main/java/com/example/wcbragas_healthplusclinic_app/MainActivity.java
@@ -2,19 +2,27 @@ package com.example.wcbragas_healthplusclinic_app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 
-    public void onNextButtonClick(View view) {
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
 }
